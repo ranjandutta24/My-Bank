e4a069329bb450b41b042994f2e41ccbf9d16ea8
Ranjan Dutta <connecttoranjan@gmail.com>
2022-11-25 20:00:35 +0530

    login

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-'use strict';
+// 'use strict';
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
-// BANKIST APP
+// MY BANK APP
 
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
+  owner: 'Ranjan Dutta',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
+  owner: 'Koushik Dutta',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
 };
 
 const account3 = {
-  owner: 'Steven Thomas Williams',
+  owner: 'Anup Kumar Shingh',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
 };
 
 const account4 = {
-  owner: 'Sarah Smith',
+  owner: 'Durga Roy',
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
@@ -71,6 +71,58 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
+const displayMovements = movments => {
+  containerMovements.innerHTML = '';
+
+  movments.forEach((mov, i) => {
+    const type = mov > 0 ? `deposit` : `withdrawal`;
+    const html = `  
+    <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__value">${mov}</div>
+    </div>`;
+    containerMovements.insertAdjacentHTML(`afterbegin`, html);
+  });
+};
+
+displayMovements(account1.movements);
+const userName = `Steven Thomas Williams`;
+
+const computeUserName = accs => {
+  accs.forEach(
+    acc =>
+      (acc.username = acc.owner
+        .toLowerCase()
+        .split(' ')
+        .map(name => name[0])
+        .join(''))
+  );
+  return accs;
+};
+console.log(computeUserName(accounts));
+
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
+const calcTotal = mov => {
+  labelBalance.textContent = mov.reduce((acc, cur) => acc + cur, 0);
+  return mov.reduce((acc, cur) => acc + cur, 0);
+};
+
+// const calcTotal = movements.reduce((acc, cur) => acc + cur, 0);
+
+console.log(calcTotal(account1.movements));
+
 /////////////////////////////////////////////////
+let currentAccount;
+btnLogin.addEventListener('click', function (e) {
+  e.preventDefault();
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
+  console.log(currentAccount);
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    console.log(`Login`);
+  }
+});
